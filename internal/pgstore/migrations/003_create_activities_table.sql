d934dbaf40c9c114c1a864bf517f317f102767e8
Eduardo Alexandre <sunnygolang@gmail.com>
2024-07-12 08:30:23 -0300

    feat: add activities migration

---
+++ internal/pgstore/migrations/003_create_activities_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS activities (
+    "id"           UUID          PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    "trip_id"      UUID                      NOT NULL,
+    "title"        VARCHAR(255)              NOT NULL,
+    "occurs_at"    TIMESTAMP                 NOT NULL,
+
+    FOREIGN KEY (trip_id) REFERENCES trips(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+);
+
+---- create above / drop below ----
+
+DROP TABLE IF EXISTS activities;
